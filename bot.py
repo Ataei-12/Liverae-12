ce85d3a009d17f2e1b7ac226c372558e478b2808
Ataei-12 <rahmatatayee0@gmail.com>
2026-02-21 12:42:11 +0430

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,6 +18,7 @@ CHANNEL = os.getenv("CHANNEL_USERNAME")
 
 app = Flask(__name__)
 bot = telebot.TeleBot(BOT_TOKEN, parse_mode="HTML")
+bot.send_message(CHANNEL, "✅ تست ارسال مستقیم موفق بود")
 STATE_FILE = "state.json"
 
 AF_MONTHS = [
